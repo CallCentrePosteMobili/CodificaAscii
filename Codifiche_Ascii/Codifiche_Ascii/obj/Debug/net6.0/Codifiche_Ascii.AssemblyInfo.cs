35c7d3a4c73bb54fb79f0ded661698d40fc460eb
Francesco Cangini <FRANCESCO.CANGINI@local.ittcesena.it>
2024-03-09 11:42:33 +0100

    aggiornamento

--- Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
+++ Codifiche_Ascii/Codifiche_Ascii/obj/Debug/net6.0/Codifiche_Ascii.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d91a8f8032b93fbacb0a6e447a198b0759f412")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codifiche_Ascii")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
